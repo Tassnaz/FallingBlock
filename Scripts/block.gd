6eb8da7c77a0ab3e949b7ab1baf4c56723585e5f
Arreb <tassnass2018@gmail.com>
2025-09-03 21:39:12 +0200

    2025-09-21:38
    Började lägga till random färg

--- Scripts/block.gd
+++ Scripts/block.gd
@@ -2,15 +2,34 @@ extends RigidBody2D
 
 var can_click = false
 
+
 func _ready():
+	
+	randomize()
+	
+	var colors = [
+		Color(1, 0, 0),       # Red
+		Color(1, 0.5, 0),     # Orange
+		Color(1, 1, 0),       # Yellow
+		Color(0, 1, 0),       # Green
+		Color(0, 0, 1),       # Blue
+	]
+	
+	modulate = colors.pick_random()
+	
 	freeze = false
 	
 func _physics_process(delta):
+	
+	
+	
 	if Input.is_action_just_pressed("LeftClick") && can_click == true:
 		queue_free()
 	
 	if Input.is_action_just_pressed("Reset"):
 		get_tree().reload_current_scene()
+	
+	#print(global_position)
 
 func _on_mouse_box_mouse_entered():
 	can_click = true
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,5 +1,38 @@
 extends Node2D
 
-@onready var block_load = load("res://Scenes/block.tscn")
+@onready var BlockLoad = load("res://Scenes/block.tscn")
 
 @onready var RandNr = RandomNumberGenerator.new()
+
+var SimpleCountdownTimer
+
+var SpawnNegX = -490
+var SpawnPosX = 534
+var SpawnPosY = -170
+
+var RandX
+
+@export var RespawnTime = 0.5
+
+func _ready():
+	SimpleCountdownTimer = RespawnTime
+
+func generate_Randx():
+	RandX = RandNr.randf_range(SpawnNegX, SpawnPosX)
+
+func _physics_process(delta):
+	if SimpleCountdownTimer >= 0:
+		SimpleCountdownTimer = SimpleCountdownTimer -1 * delta
+		
+	else:
+		
+		generate_Randx()
+		
+		var NewBlock = BlockLoad.instantiate()
+		
+		NewBlock.position.x = RandX
+		NewBlock.position.y = SpawnPosY
+		
+		add_child(NewBlock)
+		
+		SimpleCountdownTimer = RespawnTime
